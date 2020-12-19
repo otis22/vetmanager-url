0f4b759ad1f55988f1c389f65c3303a85218595f
Vladimir ROmanichev <pro100print@yahoo.com>
2020-12-19 16:03:54 +0200

    improved Domain class for parsing urls and hosts

--- src/Url/Part/Domain.php
+++ src/Url/Part/Domain.php
@@ -11,23 +11,55 @@ final class Domain implements Stringify
     /**
      * @var string
      */
-    private $domain;
+    private $urlDomainOrHostName;
 
     /**
      * Domain constructor.
-     *
-     * @param string $domain
+     * @param string $urlDomainOrHostName
      */
-    public function __construct(string $domain)
+    public function __construct(string $urlDomainOrHostName)
     {
-        $this->domain = $domain;
+        $this->urlDomainOrHostName = $urlDomainOrHostName;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function asString(): string
+    {
+        return $this->isUrl()
+            ? $this->domainFromHost($this->hostFromUrl())
+            : $this->domainFromHost($this->urlDomainOrHostName);
+    }
+
+    /**
+     * @return bool
+     */
+    private function isUrl(): bool
+    {
+        return strpos($this->urlDomainOrHostName, 'http') === 0
+            && strpos($this->urlDomainOrHostName, '.') !== false;
     }
 
     /**
      * @return string
      */
-    public function asString(): string
+    private function hostFromUrl(): string
     {
-        return $this->domain;
+        $parsed = parse_url($this->urlDomainOrHostName);
+        if (is_array($parsed) && isset($parsed['host'])) {
+            return $parsed['host'];
+        }
+        throw new \UnexpectedValueException("Can't parse url or host string - " . $this->urlDomainOrHostName);
+    }
+
+    /**
+     * @param string $host
+     * @return string
+     */
+    private function domainFromHost(string $host): string
+    {
+        $exploded = explode('.', $host);
+        return $exploded[0];
     }
 }
--- tests/Url/Part/DomainTest.php
+++ tests/Url/Part/DomainTest.php
@@ -8,11 +8,49 @@ use PHPUnit\Framework\TestCase;
 
 class DomainTest extends TestCase
 {
-    public function testDomainToString(): void
+    public function testSimpleDomainToString(): void
     {
         $this->assertEquals(
             "test",
             (new Domain("test"))->asString()
         );
+    }
+
+    public function testDomainFromHttpUrlToString(): void
+    {
+        $this->assertEquals(
+            "test",
+            (
+                new Domain("http://test.vetmanager.ru")
+            )->asString()
+        );
+    }
+
+    public function testDomainFromHttpsUrlToString(): void
+    {
+        $this->assertEquals(
+            "test",
+            (
+                new Domain("https://test.vetmanager.ru")
+            )->asString()
+        );
+    }
+
+    public function testDomainFromHostNameToString(): void
+    {
+        $this->assertEquals(
+            "test",
+            (
+                new Domain("test.vetmanager.ru")
+            )->asString()
+        );
+    }
+
+    public function testInvalidUrlThrowExceptionToString(): void
+    {
+        $this->expectException(\UnexpectedValueException::class);
+        (
+            new Domain("httpfsdfsdtest.")
+        )->asString();
     }
 }
